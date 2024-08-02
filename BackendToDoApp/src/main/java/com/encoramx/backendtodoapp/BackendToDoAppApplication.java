648f8bf059ed922dfb72a89f213ee29e86be16d7
alonso fabila <jorge.fabila@encora.com>
2024-08-01 23:12:52 -0600

    Initial commit

---
+++ BackendToDoApp/src/main/java/com/encoramx/backendtodoapp/BackendToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.encoramx.backendtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendToDoAppApplication.class, args);
+	}
+
+}
