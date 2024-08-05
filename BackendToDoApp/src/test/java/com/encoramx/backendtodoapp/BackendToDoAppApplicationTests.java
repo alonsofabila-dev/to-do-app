76d5537c5f387b3542ea1ee69daefe361cab0fad
alonso fabila <jorge.fabila@encora.com>
2024-08-05 15:18:12 -0600

    (chore): Test to check Task model.

--- BackendToDoApp/src/test/java/com/encoramx/backendtodoapp/BackendToDoAppApplicationTests.java
+++ BackendToDoApp/src/test/java/com/encoramx/backendtodoapp/BackendToDoAppApplicationTests.java
@@ -1,13 +1,44 @@
 package com.encoramx.backendtodoapp;
 
+import com.encoramx.backendtodoapp.entities.Task;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BackendToDoAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void shouldCreateNewTaskObject() {
+		Task task = new Task();
+		task.setContent("Hello World");
+		task.setPriority(Task.Priority.HIGH);
+		task.setCompleted(false);
+		task.setDueDate(LocalDateTime.of(2024, 8, 5, 8, 41, 59));
+
+		assertThat(task.getId()).isEqualTo(1);
+		assertThat(task.getContent()).isEqualTo("Hello World");
+		assertThat(task.getPriority()).isEqualTo(Task.Priority.HIGH);
+		assertThat(task.isCompleted()).isEqualTo(false);
+		assertThat(task.getDueDate()).isEqualTo(LocalDateTime.of(2024, 8, 5, 8, 41, 59));
+		assertThat(task.getCreationDate()).isNotNull();
 	}
 
+
+	@Test
+	void shouldCreateNewTaskObjectWithoutDueDate() {
+		Task task = new Task();
+		task.setContent("Hello World");
+		task.setPriority(Task.Priority.HIGH);
+		task.setCompleted(false);
+
+		assertThat(task.getId()).isEqualTo(1);
+		assertThat(task.getContent()).isEqualTo("Hello World");
+		assertThat(task.getPriority()).isEqualTo(Task.Priority.HIGH);
+		assertThat(task.isCompleted()).isEqualTo(false);
+		assertThat(task.getCreationDate()).isNotNull();
+	}
 }
