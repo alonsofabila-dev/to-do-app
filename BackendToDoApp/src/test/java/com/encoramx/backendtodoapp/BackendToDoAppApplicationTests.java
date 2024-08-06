8e3d609294154bfd49175206fc5af7eec7e7c280
alonso fabila <jorge.fabila@encora.com>
2024-08-06 10:19:20 -0600

    (fix): adjust and creation of tests for POST endpoint.

--- BackendToDoApp/src/test/java/com/encoramx/backendtodoapp/BackendToDoAppApplicationTests.java
+++ BackendToDoApp/src/test/java/com/encoramx/backendtodoapp/BackendToDoAppApplicationTests.java
@@ -1,44 +1,86 @@
 package com.encoramx.backendtodoapp;
 
-import com.encoramx.backendtodoapp.entities.Task;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import com.encoramx.backendtodoapp.entities.Task;
+
+// import com.jayway.jsonpath.DocumentContext;
+// import com.jayway.jsonpath.JsonPath;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+// import java.net.URI;
 
 import java.time.LocalDateTime;
 
-@SpringBootTest
+// import net.minidev.json.JSONArray;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendToDoAppApplicationTests {
 
-	@Test
-	void shouldCreateNewTaskObject() {
-		Task task = new Task();
-		task.setContent("Hello World");
-		task.setPriority(Task.Priority.HIGH);
-		task.setCompleted(false);
-		task.setDueDate(LocalDateTime.of(2024, 8, 5, 8, 41, 59));
+	@Autowired
+	TestRestTemplate restTemplate;
 
-		assertThat(task.getId()).isGreaterThan(0);
-		assertThat(task.getContent()).isEqualTo("Hello World");
-		assertThat(task.getPriority()).isEqualTo(Task.Priority.HIGH);
-		assertThat(task.isCompleted()).isEqualTo(false);
-		assertThat(task.getDueDate()).isEqualTo(LocalDateTime.of(2024, 8, 5, 8, 41, 59));
-		assertThat(task.getCreationDate()).isNotNull();
+
+	@Test
+	@DirtiesContext
+	void shouldCreateNewTaskObject() {
+		Task newTask = new Task(
+				"Lorem ipsum dolor sit amet",
+				Task.Priority.HIGH,
+				false,
+				LocalDateTime.of(2024, 8, 5, 8, 41, 59)
+		);
+
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/tasks", newTask, Void.class);
+
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 	}
 
 
 	@Test
+	@DirtiesContext
 	void shouldCreateNewTaskObjectWithoutDueDate() {
-		Task task = new Task();
-		task.setContent("Hello World");
-		task.setPriority(Task.Priority.HIGH);
-		task.setCompleted(false);
+		Task newTask = new Task(
+				"Lorem ipsum dolor sit amet",
+				Task.Priority.HIGH,
+				false
+		);
 
-		assertThat(task.getId()).isGreaterThan(0);
-		assertThat(task.getContent()).isEqualTo("Hello World");
-		assertThat(task.getPriority()).isEqualTo(Task.Priority.HIGH);
-		assertThat(task.isCompleted()).isEqualTo(false);
-		assertThat(task.getCreationDate()).isNotNull();
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/tasks", newTask, Void.class);
+
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+	}
+
+
+	@Test
+	@DirtiesContext
+	void shouldNotCreateNewTaskObjectWithExceedingContent() {
+		Task newTask = new Task(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nibh urna, " +
+						"vestibulum sit amet mauris a, aliquam faucibus orci. Aliquam nec feugiat diam. Vestibulum " +
+						"efficitur venenatis justo et convallis. Mauris elementum velit eget metus bibendum, eget" +
+						" laoreet lorem lacinia. Cras pellentesque pellentesque tincidunt. Fusce vehicula nec dui" +
+						" sit amet varius. Morbi fermentum mollis magna ut ultrices. Vivamus justo massa, malesuada " +
+						"pellentesque arcu eu, condimentum consectetur turpis. Praesent quis auctor lacus, id " +
+						"elementum quam. Sed rhoncus enim vel dolor sodales dignissim. Phasellus posuere facilisis mi " +
+						"eget semper. Duis pharetra ex sem.",
+				Task.Priority.HIGH,
+				false,
+				LocalDateTime.of(2024, 8, 5, 8, 41, 59)
+		);
+
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/tasks", newTask, Void.class);
+
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
 	}
 }
