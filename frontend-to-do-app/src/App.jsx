08b2ec2ff7d0f15a059b4c4dc3b80208adc25d6d
alonso fabila <jorge.fabila@encora.com>
2024-08-08 20:02:01 -0600

    (fix): TaskList component changed to be a table.

--- frontend-to-do-app/src/App.jsx
+++ frontend-to-do-app/src/App.jsx
@@ -5,10 +5,10 @@ import { Toaster } from "react-hot-toast";
 function App() {
 
   return (
-      <>
+      <div className="container mx-auto">
           <ToDosPage />
           <Toaster />
-      </>
+      </div>
   )
 }
 
--- frontend-to-do-app/src/components/TaskList.jsx
+++ frontend-to-do-app/src/components/TaskList.jsx
@@ -1,9 +1,8 @@
 import api from "../api/api.js";
-import { TOAST_ERROR_STYLE, TOAST_SUCCESS_STYLE } from "../constants.js";
-import { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
-import { List } from 'antd';
-
+import {TOAST_ERROR_STYLE, TOAST_SUCCESS_STYLE} from "../constants.js";
+import {useEffect, useState} from "react";
+import {toast} from "react-hot-toast";
+import {Space, Table} from "antd";
 
 
 export function TaskList() {
@@ -32,32 +31,72 @@ export function TaskList() {
             toast.success(response.data, TOAST_SUCCESS_STYLE);
         }).catch(error => {
             toast.error(error.message, TOAST_ERROR_STYLE);
-        })
+        });
     }
 
+    const formatDate = (dateToFormat) => {
+        if (!dateToFormat) {
+            return dateToFormat
+        }
+        return new Date(dateToFormat).toLocaleDateString('en-CA');
+    }
+
+    const columns = [
+        {
+            title: <div className="text-center">Done</div>,
+            dataIndex: 'completed',
+            key: 'completed',
+            render: (completed, record) => (
+                <div className="flex items-center justify-center">
+                    <input
+                        type="checkbox"
+                        className="w-4 h-4"
+                        checked={completed}
+                        onChange={(e) => handleCheckboxChange(record.id, e.target.checked)}
+                    />
+                </div>
+            ),
+        },
+        {
+            title: 'Name',
+            dataIndex: 'content',
+            key: 'content',
+            render: (content, record) => (
+                <span className={record.completed ? 'line-through text-gray-500' : ''}>
+                {content}
+            </span>
+            ),
+        },
+        {
+            title: 'Priority',
+            dataIndex: 'priority',
+            key: 'priority',
+        },
+        {
+            title: 'Due To',
+            dataIndex: 'dueDate',
+            key: 'dueDate',
+            render: (dueDate) => formatDate(dueDate)
+        },
+        {
+            title: <div className="text-center">Actions</div>,
+            key: 'a',
+            render: () => (
+                <Space size="middle" className="flex items-center justify-center">
+                    <button className="bg-blue-400 hover:bg-blue-600 text-white py-2 px-4 rounded">
+                        Edit
+                    </button>
+                    <button className="bg-red-400 hover:bg-red-600 text-white py-2 px-4 rounded">
+                        Delete
+                    </button>
+                </Space>),
+        },
+    ];
+
     return (
-        <div>
-            <h1>TaskList</h1>
+        <div className="bg-white rounded-lg shadow-md p-4">
             {tasks.length > 0 ?
-                <List
-                    dataSource={tasks}
-                    renderItem={(task) => (
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={
-                                    <input
-                                        type="checkbox"
-                                        checked={task.completed}
-                                        onChange={(e) => handleCheckboxChange(task.id, e.target.checked)}
-                                    />
-                                }
-                                title={task.content}
-                                description={`Due to: ${task.dueDate}`}
-                            />
-                            <p style={{ marginLeft: '16px', fontWeight: 'bold' }}>{task.priority}</p>
-                        </List.Item>
-                    )}
-                />
+                <Table dataSource={tasks} columns={columns} pagination={false}  />
                 :
                 <div>Empty Tasks</div>
             }
